013ce824ca0425d02f7632a0084a7c7f24fe61b5
Harli Kodasma <harlikodasma@gmail.com>
2025-03-15 18:23:16 +0100

    Initial commit

---
+++ spring-backend/src/test/java/hr/algebra/springbackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
