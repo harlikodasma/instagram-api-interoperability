a767f86d93077a3f7462e75efa235cd8c54cc009
Harli Kodasma <harlikodasma@gmail.com>
2025-03-20 18:29:43 +0100

    Change API URL

---
+++ spring-backend/src/main/java/hr/algebra/springbackend/rest/model/User.java
@@ -0,0 +1,38 @@
+package hr.algebra.springbackend.rest.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(name = "username", nullable = false)
+  private String username;
+
+  @Column(name = "password", nullable = false)
+  private String password;
+
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+}
---
+++ spring-backend/src/main/java/hr/algebra/springbackend/rest/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package hr.algebra.springbackend.rest.repository;
+
+import hr.algebra.springbackend.rest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
