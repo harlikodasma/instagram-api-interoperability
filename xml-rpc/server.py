7a545f4ecf6593cdac4c84189c7fe53195ba8245
Harli Kodasma <harlikodasma@gmail.com>
2025-04-02 00:55:49 +0200

    Fix CORS problems for XML-RPC server

--- xml-rpc/server.py
+++ xml-rpc/server.py
@@ -1,6 +1,17 @@
-from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
 import requests
 from xml.etree import ElementTree as ET
+
+class CORSXMLRPCRequestHandler(SimpleXMLRPCRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', 'http://localhost:4200')
+        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Authorization, Content-Type')
+        super().end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200, "OK")
+        self.end_headers()
 
 def get_temperatures(city_name):
     response = requests.get("https://vrijeme.hr/hrvatska_n.xml")
@@ -15,7 +26,7 @@ def get_temperatures(city_name):
 
     return result
 
-server = SimpleXMLRPCServer(("localhost", 8000))
+server = SimpleXMLRPCServer(("localhost", 8000), requestHandler=CORSXMLRPCRequestHandler)
 server.register_function(get_temperatures, "getTemperatures")
 print("Server started on port 8000")
 server.serve_forever()
