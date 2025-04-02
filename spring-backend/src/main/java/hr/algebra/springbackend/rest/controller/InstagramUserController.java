1b6c43810505c76c23c8a62ed8e00d1a0ad92e7b
Harli Kodasma <harlikodasma@gmail.com>
2025-04-02 03:06:58 +0200

    Handle errors, enhance returned HTTP statuses

--- spring-backend/src/main/java/hr/algebra/springbackend/rest/controller/InstagramUserController.java
+++ spring-backend/src/main/java/hr/algebra/springbackend/rest/controller/InstagramUserController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,6 +38,7 @@ public class InstagramUserController {
   }
 
   @DeleteMapping("{id}")
+  @ResponseStatus(NO_CONTENT)
   public void deleteUserFollowers(@PathVariable String username, @PathVariable Long id) {
     instagramUserService.deleteUserFollowers(username, id);
   }
--- spring-backend/src/main/java/hr/algebra/springbackend/rest/service/InstagramUserService.java
+++ spring-backend/src/main/java/hr/algebra/springbackend/rest/service/InstagramUserService.java
@@ -2,21 +2,25 @@ package hr.algebra.springbackend.rest.service;
 
 import hr.algebra.springbackend.model.jpa.InstagramUserFollower;
 import hr.algebra.springbackend.repository.InstagramUserFollowerRepository;
+import hr.algebra.springbackend.rest.exception.RestClientException;
 import hr.algebra.springbackend.rest.model.instagram.user.followers.UserFollowersDto;
 import hr.algebra.springbackend.rest.util.XmlConversionUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import static java.lang.String.format;
+import static java.util.Objects.nonNull;
 import static org.springframework.http.HttpMethod.GET;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class InstagramUserService {
 
   @Value("${rapidapi.key}")
@@ -40,11 +44,18 @@ public class InstagramUserService {
   }
 
   public UserFollowersDto modifyUserFollowers(String username, Long id, UserFollowersDto userFollowers) {
-    String latestXml = instagramUserFollowerRepository.getLatestByUsername(username).getXml();
-    return instagramUserFollowerRepository.save(new InstagramUserFollower(id, username, latestXml, userFollowers)).getJson();
+    try {
+      InstagramUserFollower latestUserFollower = instagramUserFollowerRepository.getLatestByUsername(username);
+      if (nonNull(latestUserFollower)) {
+        String latestXml = instagramUserFollowerRepository.getLatestByUsername(username).getXml();
+        return instagramUserFollowerRepository.save(new InstagramUserFollower(id, username, latestXml, userFollowers)).getJson();
+      }
+      throw new RestClientException("Username not found");
+    } catch (ObjectOptimisticLockingFailureException e) {
+      throw new RestClientException("Object not found");
+    }
   }
 
-  @Transactional
   public void deleteUserFollowers(String username, Long id) {
     instagramUserFollowerRepository.deleteByUsernameAndId(username, id);
   }
@@ -53,7 +64,11 @@ public class InstagramUserService {
     String url = format("https://%s/v1/user_followers_adv?username_or_id=%s", RAPIDAPI_HOST, username);
     HttpEntity<String> httpEntity = createHttpEntityWithHeaders();
 
-    return restTemplate.exchange(url, GET, httpEntity, UserFollowersDto.class).getBody();
+    try {
+      return restTemplate.exchange(url, GET, httpEntity, UserFollowersDto.class).getBody();
+    } catch (org.springframework.web.client.RestClientException e) {
+      throw new RestClientException("User not found");
+    }
   }
 
   private HttpEntity<String> createHttpEntityWithHeaders() {
